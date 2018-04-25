4a4500536036cbb182bc870b0fd277b46487ce56
angiiemv13@gmail.com <angiiemv13@gmail.com>
2018-04-25 15:46:09 +0000

    Se agrega la variable para el incremento

--- ejemplo-1-calentador.cpp
+++ ejemplo-1-calentador.cpp
@@ -4,17 +4,25 @@ class Calentador
 {
   private: 
         int temperatura;
+        int incremento;
   
   public:
         Calentador();
         void calentar();
         void enfriar();
         void imprimeTemperatura();
+        int accedeTemperatura() const; //se le agrega a los met. de acceso, significa que no modifica las variables.
 };// punto y coma obligatorio
 
 Calentador::Calentador()
 {
     temperatura=15;
+    incremento=3;
+}
+
+int Calentador::accedeTemperatura() const
+{
+    return temperatura;
 }
 
 void Calentador::calentar()
